c7356340ed0ab9ca67df217284107ef3d2d85afc
Antonorlov <66281524+antonaorlov@users.noreply.github.com>
2024-03-04 07:33:45 -0500

    Update Laser.cpp

--- Laser.cpp
+++ Laser.cpp
@@ -1,22 +1,25 @@
 #include "Laser.hpp"
 #include <cmath>
+//laser with position, rotation, speed
 Laser::Laser(const sf::Vector2f& position, float rotation, float speed): speed(speed){
-    shape.setSize(sf::Vector2f(10.f,2.f));
-    shape.setOrigin(shape.getSize().x/2, shape.getSize().y/2);
+    shape.setSize(sf::Vector2f(10.f,2.f)); //size of laser
+    shape.setOrigin(shape.getSize().x/2, shape.getSize().y/2); //origin of laser
     shape.setPosition(position);
     shape.setRotation(rotation);
     shape.setFillColor(sf::Color::Blue);
 }
 
+//update laser with speed, direction
 void Laser::Update(){
     float angle=shape.getRotation()*3.14159f/180.f; //radians
     shape.move(speed*cos(angle), speed*sin(angle));
 }
 
+//draw
 void Laser::draw(sf::RenderWindow& window){
     window.draw(shape);
 }
-
+//check if ofscreen
 bool Laser::offScreen(const sf::RenderWindow & window) const{
     sf::Vector2f position = shape.getPosition();
     return position.x <0 || position.x>window.getSize().x ||
