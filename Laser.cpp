ccd0516a3ab47537a7228d5c16da01581d4bf0e4
Antonorlov <66281524+antonaorlov@users.noreply.github.com>
2024-03-04 14:40:55 -0500

    Update Laser.cpp

--- Laser.cpp
+++ Laser.cpp
@@ -1,5 +1,9 @@
 #include "Laser.hpp"
+
+//utuilized resources from SFML docs https://www.sfml-dev.org/learn.php
+
 #include <cmath>
+
 //laser with position, rotation, speed
 Laser::Laser(const sf::Vector2f& position, float rotation, float speed): speed(speed){
     shape.setSize(sf::Vector2f(10.f,2.f)); //size of laser
@@ -11,17 +15,19 @@ Laser::Laser(const sf::Vector2f& position, float rotation, float speed): speed(s
 
 //update laser with speed, direction
 void Laser::Update(){
-    float angle=shape.getRotation()*3.14159f/180.f; //radians
-    shape.move(speed*cos(angle), speed*sin(angle));
+    float angle=shape.getRotation()*3.14159f/180.f; //change rotation angle to radians
+    shape.move(speed*cos(angle), speed*sin(angle)); //move the laser in direction its pointing
 }
 
-//draw
+//draw laser in sfml
 void Laser::draw(sf::RenderWindow& window){
     window.draw(shape);
 }
+
 //check if ofscreen
 bool Laser::offScreen(const sf::RenderWindow & window) const{
     sf::Vector2f position = shape.getPosition();
+    //returns true if laser is offscreen
     return position.x <0 || position.x>window.getSize().x ||
            position.y<0  || position.y >window.getSize().y;
 }
