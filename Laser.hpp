c578f65f78805992084a475e1666de023059b989
Antonorlov <66281524+antonaorlov@users.noreply.github.com>
2024-03-04 07:31:21 -0500

    Update Laser.hpp

--- Laser.hpp
+++ Laser.hpp
@@ -5,16 +5,18 @@
 
 class Laser{
     public:
+        //constructor new laser with position, rotation, speed
         Laser(const sf::Vector2f& position, float rotation, float speed);
-
+        //update laser constructor
         void Update();
+        //draws laser
         void draw(sf::RenderWindow& window);
-
+        //chekc if laser is off screen then return a boolean
         bool offScreen(const sf::RenderWindow & window) const;
     
     private:
-        sf::RectangleShape shape;
-        float speed;
+        sf::RectangleShape shape; //laser
+        float speed; //speed
 };
 
 #include "Laser.cpp"
