8df9c14ca32caf6f8a129dfb21be968aae6e1e54
Antonorlov <66281524+antonaorlov@users.noreply.github.com>
2024-03-04 14:33:03 -0500

    Update Lives.cpp

--- Lives.cpp
+++ Lives.cpp
@@ -1,5 +1,5 @@
 #include "Lives.hpp"
-
+//constructor count total amount of lives, get the sprite 
 Lives::Lives(int count, const std::string& heartImage):lives(count){
     if(!heartTexture.loadFromFile(heartImage)){
         throw std::runtime_error("Failed to load heart Image");
@@ -14,20 +14,20 @@ Lives::Lives(int count, const std::string& heartImage):lives(count){
         hearts.push_back(heart);
     }
 }
-
+//se position of hearts
 void Lives::setPosition(float x, float y){
     float spacing=10.0f;
     for(size_t i=0; i<hearts.size(); i++){
         hearts[i].setPosition(x+i*(heartTexture.getSize().x * hearts[i].getScale().x + spacing), y);
     }
 }
-
+//draw hearts
 void Lives::draw(sf::RenderWindow& window){
     for(int i=0;i<lives;i++){
         window.draw(hearts[i]);
     }
 }
-
+//resize the hearts if needed
 float Lives::getHeartWidth() const {
     return heartTexture.getSize().x;
 }
